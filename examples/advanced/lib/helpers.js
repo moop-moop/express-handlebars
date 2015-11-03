cb764a91ca382a6f126ca586022567af1f1f905e
Nathan Morris <morris.nathan@gmail.com>
2015-11-03 10:58:55 -0600

    Add block and partial for template inheritance

--- examples/advanced/lib/helpers.js
+++ examples/advanced/lib/helpers.js
@@ -1,5 +1,26 @@
 'use strict';
+var Handlebars = require('handlebars');
 
 exports.yell = function (msg) {
     return msg.toUpperCase();
 };
+
+Handlebars.loadPartial = function (name) {
+  var partial = Handlebars.partials[name];
+  if (typeof partial === "string") {
+    partial = Handlebars.compile(partial);
+    Handlebars.partials[name] = partial;
+  }
+  return partial;
+};
+
+exports.block = function (name, options) {
+    /* Look for partial by name. */
+    var partial
+      = Handlebars.loadPartial(name) || options.fn;
+    return partial(this, { data : options.hash });
+ };
+
+exports.partial = function (name, options) {
+    Handlebars.registerPartial(name, options.fn);
+ };
